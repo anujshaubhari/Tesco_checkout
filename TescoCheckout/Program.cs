46118a3f371432546d51ad56eab293d95e112084
Anuj Shaubhari <va_ashaubhari@australiancricket.onmicrosoft.com>
2022-08-17 19:31:31 +0530

    Add project files.

---
+++ TescoCheckout/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TescoCheckout
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
